bb1f191d2927e1d48ee351bdfbc8e66162219d0f
trebek1 <trebek08@gmail.com>
2015-07-26 21:17:37 -0700

    add votes

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ app.controller('MainCtrl', ['$scope',function($scope){
   $scope.test = 'Hello world!';
   $scope.posts = [{title: 'post 1', upvotes: 5}, {title: 'post 2', upvotes: 2},  {title: 'post 3', upvotes: 15}, {title: 'post 4', upvotes: 9},{title: 'post 5', upvotes: 4}];
   $scope.addPost = function(){
-  	$scope.posts.push({title: "A new post", upvotes: 0}); 
-  };
+  	if(!$scope.title || $scope.title === ''){
+  	 return; 
+  	}
+  	$scope.posts.push({title: $scope.title, upvotes: 0}); 
+  	$scope.title = '';
+  	};
+  	$scope.incrementUpvotes = function(post){
+  		post.upvotes += 1; 
+  	};
+  
 }]);	
