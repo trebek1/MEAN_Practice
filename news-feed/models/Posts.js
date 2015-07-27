b1d307cac1749122cafe26432aab8080b840f8d1
trebek1 <trebek08@gmail.com>
2015-07-27 13:14:11 -0700

    added get, post and upvote routes

--- news-feed/models/Posts.js
+++ news-feed/models/Posts.js
@@ -7,4 +7,10 @@ upvotes: {type: Number, default: 0},
 comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
+PostSchema.methods.upvote = function(cb){
+
+	this.upvotes +=1;
+	this.save(cb);
+}
+
 mongoose.model('Post', PostSchema)
--- news-feed/routes/index.js
+++ news-feed/routes/index.js
@@ -6,4 +6,48 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post'); 
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req,res, next){
+	Post.find(function(err, posts){
+		if(err) { return next(err); }
+		res.json(posts);
+	});
+});
+
+router.post('/posts/', function(req,res, next){
+
+	var post = new Post(req.body);
+	post.save(function(err,post){
+		if(err){ return next(err); }
+		res.json(post);
+	});
+
+});
+
+router.param('post', function(req,res,next,id){
+	var query = Post.findById(id);
+
+	query.exec(function(err,post){
+		if(err){ return next(err);}
+		if(!post){ return next(new Error( "Cant find post "));}
+
+		req.post = post; 
+		return next(); 
+	})
+})
+
+router.get('/posts/:post', function(req,res){
+	res.json(req.post);
+})
+
+router.put('/posts/:post/upvote', function(req,res,next){
+req.post.upvote(function(err,post){
+	if(err){return next(err);}
+		res.json(post);
+})
+})
+
 module.exports = router;
