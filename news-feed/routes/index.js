8143944c2279c682ec451af87cada39f8ea14af9
trebek1 <trebek08@gmail.com>
2015-07-27 13:27:52 -0700

    finished backend

--- news-feed/routes/index.js
+++ news-feed/routes/index.js
@@ -39,15 +39,51 @@ router.param('post', function(req,res,next,id){
 	})
 })
 
-router.get('/posts/:post', function(req,res){
-	res.json(req.post);
+router.param('comment', function(req,res,next,id){
+	var query = Comment.findById(id);
+
+	query.exec(function(err,comment){
+		if(err){ return next(err);}
+		if(!comment){ return next(new Error( "Cant find comment "));}
+
+		req.comment = comment; 
+		return next(); 
+	})
 })
 
+router.get('/posts/:post', function(req,res){
+	req.post.populate('comments', function(err,post){
+		req.json(post);
+	});
+});
+
 router.put('/posts/:post/upvote', function(req,res,next){
-req.post.upvote(function(err,post){
-	if(err){return next(err);}
-		res.json(post);
-})
-})
+	req.post.upvote(function(err,post){
+		if(err){return next(err);}
+			res.json(post);
+	});
+});
+
+router.put('/posts/:post:/comments/:comment/upvote', function(req,res,next){
+	req.comment.upvote(function(err,comment){
+		if(err){return next(err);}
+			res.json(comment);
+	});
+});
+
+
+router.post('/posts/:post/comments', function(req,res,next){
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+	comment.save(function(err,comment){
+		if(err){ return next(err);}
+		req.post.comments.push(comment); 
+		req.post.save(function(err, post){
+			if(err){ return next(err);}
+			res.json(comment);
+		});
+	});
+});
+
 
 module.exports = router;
